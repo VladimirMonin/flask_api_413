b8467dcc45e33bf3040651d979df80df99052f28
VladimirMonin <vladimir.monin2016@yandex.ru>
2025-05-30 21:23:36 +0500

    lesson_45: группа по id и функция сериализации в JSON

--- utils.py
+++ utils.py
@@ -1,1 +1,42 @@
-from models import *
+from models import Groups
+from peewee import DoesNotExist
+import json
+import datetime
+
+# Функция для получения группы по id
+# groups/<id>
+def get_group_by_id(group_id):
+    try:
+        group = Groups.get(Groups.id == group_id)
+        return group
+    except DoesNotExist:
+        return None
+
+
+# Как бы могла выглядить функция для сериализации в JSON одного объекта с неопределенным количеством полей?
+def serialize_model_instance(instance):
+    """
+    Сериализует объект модели в словарь, включая только определенные поля.
+    """
+    serialized_data = {}
+    
+    # _meta.sorted_fields позволяет получить все поля модели в порядке их определения
+    for field in instance._meta.sorted_fields:
+        field_data = getattr(instance, field.name)
+        
+        # Проверяем если данные являются объектом даты или времени- превращаем их в строку
+        if isinstance(field_data, (datetime.date, datetime.datetime)):
+            serialized_data[field.name] = field_data.isoformat()
+        else:
+            serialized_data[field.name] = field_data
+    
+    json_data = json.dumps(serialized_data, indent=4, ensure_ascii=False)
+    return json_data
+
+# Протестируем на группе 1
+if __name__ == "__main__":
+    group = get_group_by_id(1)
+    if group:
+        print("Группа найдена:\n", serialize_model_instance(group))
+    else:
+        print("Группа не найдена.")
